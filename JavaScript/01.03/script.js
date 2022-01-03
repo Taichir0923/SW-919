a7386ae088722772d12a57491a15334be7ddba3f
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-01-03 00:24:35 -0500

    Update script.js

--- JavaScript/01.03/script.js
+++ JavaScript/01.03/script.js
@@ -1,1 +1,72 @@
 // github.com/taichir0923/SW-919
+
+// JavaScript - Multiparadigm 
+// JavaScript 
+// static - dynamic
+
+// dynamic web page
+// web application - react library
+// mobile app - react
+// desktop app - typescript , react
+
+// server side app
+// NodeJS
+
+// threeJS
+// unity
+// unreal4
+// style.css
+// script.js
+
+// JS Basics
+//  datatypes
+//      primitive datatype
+//      non-primitive datatype
+//  variables
+//  confition
+//  loop
+//  functions
+//  scope
+//  eventloop
+
+// DOM - Document Object Model
+// event
+
+// OOP - Objecte Oriented Programming
+
+
+// Asynchronous JavaScript
+// Promise
+// Fetch API
+//  request
+//  response
+
+// Datatypes
+
+
+// "" - double quotation mark
+// '' - single quotation mark
+// `` - back quote , back tic , template string iteration
+// - dash
+
+// camel case - backgroundColor
+// snake case - background_color
+
+// reserved words - Программийн хэлэнд ашиглагддаг тусгай түлхүүр үгс
+
+// 001110101010111010101110010101
+
+// Primitive Datatype - Number , String , Boolean , undefined , null
+//  1. Number - Бүх тон төрлийн өгөгдлүүд - 1234567890
+//  2. String - Текст төвтэй өгөгдлүүд - "text" , 'text' , `text`
+//  3. Boolean - true (1) , false (0)
+//  4. undefined - тодорхойгүй
+//  5. null - хоосон
+
+// Variables - Хувьсагч - Өгөгдөл хадгалах сав - санах ойд хадгалах хаяг
+// Хувьсагч зарлахад reserved word ашиглаж болохгүй. Ашиглавал ямар нэг байдлаар өөрчлилж ашиглах шаардлагатай
+// Хувьсагчийн нэр тоогоор эхэлж болохгүй
+// var ner1 = "Narada"
+// var ner
+
+// Non-Primitive DataType - Arrays , Object , Regular Expressions
