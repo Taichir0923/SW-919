4fc3db7092b96acafbb3d67eca4a68cf555f2b6e
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-01-14 00:08:56 -0500

    Update script.js

--- JavaScript/01.10/script.js
+++ JavaScript/01.10/script.js
@@ -45,5 +45,75 @@
 // * 5 оронтой тоо оруулаад бүх цифрүүдийн нийлбэрийг ол
 
 
-// "jgkdsfljllkjfd" - Хамгийн урт давтагдаагүй чар агуулсан 
-// substring-н уртыг консолд хэвлэ
+// loop - Давталт
+
+// 5 orontoi too orulad sondgoi tsifr buriig consold hevle
+
+// var x = 64327;
+
+// var xStr = x.toString();
+
+// if(xStr[0] % 2 === 1){
+//     console.log(xStr[0]);
+// }
+
+// if(xStr[1] % 2 === 1){
+//     console.log(xStr[1]);
+// }
+
+// if(xStr[2] % 2 === 1){
+//     console.log(xStr[2]);
+// }
+
+// for - operator shilglana
+
+// let x = prompt('too oruul');
+// let sum = 0;
+// let byThree = 0;
+
+// for(let i = 0; i < x.length; i++){
+//     if(+x[i] % 2 === 0){
+//         sum += +x[i];
+//     }
+
+//     if(+x[i] % 3 === 0){
+//         byThree++;
+//     }
+// }
+
+
+// oruulsan toonii tegsh tsifruudiin niilberiig ol.
+
+// oruulsan toonii huvid 3t huvaagddag heden shirheg too bgaag tool.
+
+
+// {} - scope , block
+
+// {
+//     let qwer = 34
+// }
+
+// console.log(qwer)
+
+// let x = +prompt('1-r too')
+// let y = +prompt('2-r too')
+// let z = +prompt('3-r too')
+// let hariu = 0;
+// if(x === 0){
+//     hariu++;
+// }
+
+// if(y === 0){
+//     hariu++
+// }
+
+// if(z === 0){
+//     hariu++
+// }
+
+
+// 2.r dasgal
+
+// for(let i = 0; i < 100; i++){
+//     console.log(i + ": Hello")
+// }
