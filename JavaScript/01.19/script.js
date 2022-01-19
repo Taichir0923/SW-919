f34b02c6a6b0ac990b928617a2bc1c79b17fdaf8
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-01-19 12:53:53 +0800

    1.19

---
+++ JavaScript/01.19/script.js
@@ -0,0 +1,56 @@
+// Object , Array , Regular Expressions
+// Non-Primitive Data type
+
+// [] - massiviin haalt
+
+// Data Struncture - Өгөгдлийн бүтэц
+
+// new Array() == []
+
+// var array = new Array(15 , 16 , 17 , 18 , "Narada" , true);
+
+// array methods
+
+
+// var massiv = [100 , 200 , 300 , "ghfjksdhk"];
+
+// // push - хамгийн сүүлд нь шинэ өгөгдөл нэсж өгнө.
+// massiv.push("Narada");
+// massiv.push(123);
+
+// // pop - Хамгийн сүүлийн өгөгдлийг устгаж өгнө
+// massiv.pop();
+// massiv.pop();
+// massiv.pop();
+
+// // unshift - хамгийн эхэнд нь шинэ өгөгдөл нэсж өгнө.
+// massiv.unshift(99);
+// massiv.unshift(95);
+
+// // shift - Хамгийн эхний өгөгдлийг устгаж өгнө
+// massiv.shift();
+// massiv.shift();
+
+// // splice - дундаас нь өгөгдөл утсгах
+// // splice(устгах гэж байгаа өгөгдлийн индекс , устгах тоо)
+// // massiv.splice(1 , 1)
+
+// var arr1 = [12 , 13 , 14];
+// var arr2 = [15 , 16 , 17];
+// // concat - массивуудыг өргөтгөнө
+
+// var arr3 = arr1.concat(arr2);
+
+// slice - массивын тодорхой тасдаж авна.
+// // slice(хамгийн эхний элементийн индекс , сүүлийн элементийн индекс)
+
+// for(var i = 0; i < arr3.length; i++){
+//     var x = arr3[i] * 3;
+//     console.log(x);
+// }
+
+// reverse - массивыг урвуу хэлбэрт оруулна
+// join - массив доторх өгөгдлүүдийг нэг ширхэг стринг болгож буцаана
+// join(ямар тэмдэгт ашиглахаа бичнэ)
+
+// массив
