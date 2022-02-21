0f48852e3488a7839f9ce70ec2bb61dbe18bd996
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-21 13:06:06 +0800

    Update script.js

--- JavaScript/revise/script.js
+++ JavaScript/revise/script.js
@@ -9,6 +9,44 @@
 
 // if(condition) { true } else {false}
 
-for(var i = 0; i < 10; i++){
-    
+// тооцоолол хийж тодорхой үр дүн авах
+// function myFunction(){
+//     console.log(12)
+// }
+
+// var x = function(num){
+//     return num;
+// }(123)
+
+// function nas(){
+//     return 12
+// }
+
+// var torson = 2022 - nas();
+// var torsonOn = 2022 - myFunction();
+
+// 100 hurtelh buh 7-d huvaagddag toonii niilberiig butsaadag function bich
+function sumUpTo(){
+    var sum = 0;
+    for(var i = 0; i < 100; i++){
+        if(i%7 === 0){
+            sum += i
+        }
+    }
+    return sum;
 }
+
+function sumFive(){
+    var sum = 0;
+    for(var i = 0; i < 100; i++){
+        if(i%5 === 0){
+            sum += i
+        }
+    }
+    return sum;
+}
+
+function zoruu(){
+    return Math.abs(sumUpTo() - sumFive())
+}
+// 100 hurtelh 7-d huvaagddag toonuudiin niilber bolon , buh 5-d huvaagddag toonuudiin niilberuuriin zoruug butsaah function bich
