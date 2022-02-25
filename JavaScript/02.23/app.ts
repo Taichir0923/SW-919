093559e8e6a9581ad85eb7abd880700067204a19
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-25 12:01:11 +0800

    update

---
+++ JavaScript/02.23/app.ts
@@ -0,0 +1,23 @@
+// var x:number = 23;
+// x = "Fd"
+
+
+// interface Person {
+//     ner: string,
+//     id: string,
+//     email: string,
+//     number: number
+// }
+
+// var person: Person = {
+//     ner: "Narada",
+//     id: Math.random().toString(),
+//     email: "narada@mail.com",
+//     number: 99009900
+// }
+
+// function Test<Person>(obj: Person): void{
+
+// }
+
+// mongodb 
---
+++ JavaScript/02.23/script.js
@@ -0,0 +1,7 @@
+// static typed
+// dynamic typed
+
+var nas = 23;
+
+// JS is 100% valid TypeScript
+// 
