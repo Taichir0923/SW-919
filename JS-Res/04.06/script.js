1b358bc40030614470c43f20c3f3b2fc80785d77
narada0923 <narada4027@gmail.com>
2022-04-08 13:29:23 +0800

    Intro to ES6 Syntax

--- JS-Res/04.06/script.js
+++ JS-Res/04.06/script.js
@@ -2,24 +2,108 @@
 // Object Oriented Programming Language in JavaScript
 // Constructor Function
 
-function Test (name , email) {
-    this.fullname = name;
-    this.email = email
-};
+// function Test (name , email) {
+//     this.fullname = name;
+//     this.email = email
+// };
 
-Test.prototype.testFn = function(){
-    return this.fullname;
-}
+// Test.prototype.testFn = function(){
+//     return this.fullname;
+// }
 
-Test.prototype.setName = function(arg){
-    this.fullname = arg;
-    this[arg] = arg;
-    return this.fullname;
-}
+// Test.prototype.setName = function(arg){
+//     this.fullname = arg;
+//     this[arg] = arg;
+//     return this.fullname;
+// }
 
-person1 = new Test("Ner1");
+// person1 = new Test("Ner1");
 
-data = new Test("Narada" , 'narada@mail.com');
+// data = new Test("Narada" , 'narada@mail.com');
 
 // utga=mergejil => {mergejil: mergejil};
 
+// ES6 - new generation JS , modern syntax
+// 1996 => ES1 - EcmaScript1
+// 1997 => ES2 - EcmaScript2
+// 1998 => ES3 - EcmaScript3
+
+// 2009 => ES5 - EcmaScript5
+
+
+// babel => orchin uyiin JS syntaxiig ES5,4,3 omnoh huvilbarluu horvulne
+
+// 2015 => ES6 - EcmaScript6
+// 2016 => ES7
+// 2017 => ES8
+// 2018 => ES9
+// 2019 => ES10
+// 2020 => ES11
+// 2021.06 => ES12
+
+// var
+
+// ES5 => var
+// ES6 => let , const
+// ES: block scope
+
+// string concat
+
+// string iteration , template string
+
+// let str1 = "Hi";
+// let str2 = "there.!!"
+
+// console.log("ES5: " + str1 + " " + str2);
+
+// console.log(`ES6: ${str1} ${str2}`);
+
+
+// spread operator;
+// let arr = [23 , 45 , 65];
+
+// let arr1 = [...arr , 432 , 4893 , 123];
+
+// let obj = {
+//     ner: "Narada",
+//     mergejil: "Blabla",
+//     hobby: "gjfdskl"
+// }
+
+
+// let obj1 = {
+//     ...obj,
+//     arr1
+// }
+
+// let ner = null;
+// let mergejil = "Designer";
+// let hobby = "Watching...";
+
+// let newObj = {ner , mergejil , hobby};
+
+// let [a , b , c] = [23 , 24 , 25];
+
+// let {ner , nas} = {ner: "Bat" , nas: 24};
+
+// arrow function : () => {}
+// const fn5 = function(){
+//     return [12 , 13]
+// }
+// const fn = () => (arg) => {
+//     console.log(arg)
+// }
+
+// const fn = (cb) => {
+//     cb();
+// }
+
+// fn(() => {
+//     console.log("Callback function worked")
+// });
+
+// class Test {
+//     constructor(ner){
+//         this.ner = ner;
+//     };
+// };
